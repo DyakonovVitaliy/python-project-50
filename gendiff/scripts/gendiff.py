5643ab05d8cf6bbc9e4b6adf9123b3890d809e1c
DyakonovVitaliy <dyakonovvitaliy37@gmail.com>
2023-09-03 01:38:16 +0300

    first_step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+import argparse
 
-def main():
-    print('something')
+
+def main():    
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('indir', type=str, help='first_file')
+    parser.add_argument('outdir', type=str, help='Second_file')
+    args = parser.parse_args()
+    print(args.indir)
 
 if __name__ == '__main__':
     main()
