8d6b724187c3fa0008fa4b9d365f877b3bdc1df8
DyakonovVitaliy <dyakonovvitaliy37@gmail.com>
2023-09-03 16:53:27 +0300

    second_step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('indir', type=str, help='first_file')
     parser.add_argument('outdir', type=str, help='Second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args.indir)
 
