eaf618ac9a25183de82102cc98d12584e5058247
DyakonovVitaliy <dyakonovvitaliy37@gmail.com>
2023-09-06 19:49:00 +0300

    1

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from gendiff.cli import parse
+from gendiff.diff_files import generate_diff
 
 def main():    
-    parse()
+    args = parse()
+    print(generate_diff(args.first_file, args.second.file))
 
 if __name__ == '__main__':
     main()
